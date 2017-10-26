59729d49356c0290444668b69ff77d4883664762
DanielDefta <daniel.defta.w0463@student.hogent.be>
2017-10-26 15:21:41 +0200

    sever.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(expressJwt({
         }
         return null;
     }
-}).unless({ path: ['/','/users/authenticate', '/users/register', '/users/passwordrecover', '/users/checkanswers', '/users/resetpassword'] }));
+}).unless({ path: ['/welcome','/users/authenticate', '/users/register', '/users/passwordrecover', '/users/checkanswers', '/users/resetpassword'] }));
 
 // routes
 app.use('/users', require('./controllers/users.controller'));
